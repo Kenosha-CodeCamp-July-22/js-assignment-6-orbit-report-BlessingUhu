2964f898584b6f27d66aeaedc48705092a323c41
Blessing Uhunmwangho <Bless@macbook-pro.mynetworksettings.com>
2022-08-25 22:59:59 -0400

    Finished assignment 6

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,31 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Satellite } from '../satellite';
+import { Component, OnInit, Input } from "@angular/core";
+import { Satellite } from "../satellite";
+import { enableProdMode } from "@angular/core";
+
+enableProdMode();
 
 @Component({
-  selector: 'app-orbit-counts',
-  templateUrl: './orbit-counts.component.html',
-  styleUrls: ['./orbit-counts.component.css']
+  selector: "app-orbit-counts",
+  templateUrl: "./orbit-counts.component.html",
+  styleUrls: ["./orbit-counts.component.css"],
 })
 export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
+  @Input() search;
 
-	@Input() satellites: Satellite[];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   countByType(type: string): number {
-	let count = 0;
-	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
-	  }
-	}
-	return count;
- }
-
-
+    let count: number = 0;
+    if (this.satellites) {
+      for (let i: number = 0; i < this.satellites.length; i++) {
+        if (this.satellites[i].type === type) {
+          count++;
+        }
+      }
+      return count;
+    }
+  }
 }
